ee7b6c196b9993770ea5724f54017ed209e5878a
muhammad iqbal <iqbal.muh998@gmail.com>
2022-04-27 15:38:43 +0700

    upgrade index.html add template register and login

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-$(document).ready(() => {
+function isNotLogin(){
   $('#app').hide()
+  $('#form-register').hide()
+}
+
+$(document).ready(() => {
+  isNotLogin()
+
+  $('#form-login').on('submit', () => {
+    event.preventDefault()
+    $('#app').show()
+    $('#login').hide()
+  })
+  $('.button-register').on('click', () => {
+    $('#form-register').show()
+    $('#login').hide()
+    $('.button-register').hide()
+    
+  })
+  $('.logout')
+
   console.log( "ready!" );
 });
